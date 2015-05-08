a3b5a08ab657d733d2a0e96edc006fa1a3b78638
Alexander Viklund <viklu@kth.se>
2015-05-09 00:07:38 +0200

    Add cannonball constructor

--- Kod/src/Cannonball.java
+++ Kod/src/Cannonball.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Alexander on 2015-05-08.
  */
-public class Cannonball {
+public class Cannonball extends Entity {
+    public Cannonball(Point p) {
+        super(p, new ImageIcon("resources/pictures/cannonball.png"));
+    }
+
 }
