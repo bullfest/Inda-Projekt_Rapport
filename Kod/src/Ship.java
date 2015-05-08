bbb12bb16a2aa69665bbc56ffdef15e42976b3c7
Alexander Viklund <viklu@kth.se>
2015-05-09 00:05:11 +0200

    Rename Boat.java to Ship.java

---
+++ Kod/src/Ship.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Alexander on 2015-05-08.
+ */
+public class Ship extends Entity {
+
+        public Ship(Point p){
+            super(p, new ImageIcon("resources/pictures/ship.png"));
+
+        }
+}
